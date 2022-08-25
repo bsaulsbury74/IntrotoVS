f8e9f6e3bc14ef5f7002a8c9a917dfb39cbac2a6
Brendan <brendasa7@gmail.com>
2022-08-25 13:23:52 -0500

    initial commit

---
+++ MyGraph.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.linspace(0,20,100)
+plt.plot(x,np.sin(x))
+plt.show()
+#step 1 python3- m venv .venv
+#step 2 source .venv/bin/activate in order to activate virtual environment
+#step 3 pip3 install matplotlib install libraries
